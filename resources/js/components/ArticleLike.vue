2cd19a2fd9a3974111a9203e2b69d9381d5185e2
teramoto <teramoto@co-well.jp>
2020-04-20 15:11:18 +0900

    いいね判定

--- resources/js/components/ArticleLike.vue
+++ resources/js/components/ArticleLike.vue
@@ -1,10 +1,24 @@
 <template>
     <div>
         <button type="button" class="btn m-0 p-1 shadow-none">
-            <i class="fas fa-heart mr-1"></i>
+            <i class="fas fa-heart mr-1" :class="{'red-text':this.isLikedBy}"></i>
         </button>
         10
     </div>
 </template>
 
-<script></script>
+<script>
+    export default {
+        props: {
+            initialIsLikedBy: {
+                type: Boolean,
+                default: false,
+            },
+        },
+        data() {
+            return {
+                isLikedBy: this.initialIsLikedBy,
+            }
+        },
+    }
+</script>
