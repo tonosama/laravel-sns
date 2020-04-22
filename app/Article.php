a42ef72046e45debe485add68cc53f8ddb82f670
teramoto <teramoto@co-well.jp>
2020-04-22 17:34:09 +0900

    記事投稿処理とタグモデルの編集

--- app/Article.php
+++ app/Article.php
@@ -40,4 +40,10 @@ class Article extends Model
         return $this->likes->count();
     }
 
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Tag')->withTimestamps();
+    }
+
+
 }
