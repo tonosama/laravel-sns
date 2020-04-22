b4b07c46ecec43b2bef5762e59b72aa5f47f0652
teramoto <teramoto@co-well.jp>
2020-04-22 18:03:53 +0900

    記事へのタグ表示とタグ登録の動作確認

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -5,51 +5,65 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
 use App\Article;
+use App\Tag;
+
 
 class ArticleController extends Controller
 {
 
     public function __construct()
     {
-        $this->authorizeResource(Article::class,'article');
-
+        $this->authorizeResource(Article::class, 'article');
     }
 
 
     // 一覧表示
-    public function index(){
+    public function index()
+    {
         $articles = Article::all()->sortByDesc('created_at');
         return view('articles.index', ['articles' => $articles]);
     }
 
     // 登録
-    public function create(){
+    public function create()
+    {
 
         return view('articles.create');
     }
 
-    public function edit(Article $article){
-        return view('articles.edit',['article' => $article]);
+    public function edit(Article $article)
+    {
+        return view('articles.edit', ['article' => $article]);
     }
 
-    public function store(ArticleRequest $request, Article $article){
+    public function store(ArticleRequest $request, Article $article)
+    {
         $article->fill($request->all()); //-- この行を追加
         $article->user_id = $request->user()->id;
         $article->save();
+
+        $request->tags->each(function ($tagName) use ($article) {
+            $tag = Tag::firstOrCreate(['name' => $tagName]);
+            $article->tags()->attach($tag);
+        });
+
         return redirect()->route('articles.index');
     }
 
-    public function update(ArticleRequest $request, Article $article){
+    public function update(ArticleRequest $request, Article $article)
+    {
         $article->fill($request->all())->save();
         return redirect()->route('articles.index');
     }
 
-    public function destroy(Article $article){
+    public function destroy(Article $article)
+    {
         $article->delete();
         return redirect()->route('articles.index');
     }
 
-    public function show(Article $article){
+    public function show(Article $article)
+    {
         return view('articles.show', ['article' => $article]);
     }
 
@@ -74,7 +88,6 @@ class ArticleController extends Controller
             'countLikes' => $article->count_likes,
         ];
     }
-
 
 
 }
--- app/Http/Requests/ArticleRequest.php
+++ app/Http/Requests/ArticleRequest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ArticleRequest extends FormRequest
 {
+    private $tags;
+
     /**
      * Determine if the user is authorized to make this request.
      *
