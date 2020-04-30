f63708a55ddd0de77af43806a1e1754579b74261
teramoto <teramoto@co-well.jp>
2020-04-30 17:52:56 +0900

    chapter8対応

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,6 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-//            $table->bigIncrements('id');
-//            $table->string('name');
-//            $table->string('email')->unique();
-//            $table->timestamp('email_verified_at')->nullable();
-//            $table->string('password');
-//            $table->rememberToken();
-//            $table->timestamps();
             $table->bigIncrements('id');
             $table->string('name')->unique(); //-- この行を変更
             $table->string('email')->unique();
